d5278b6d536c3371c95ed3a3bf65114d7329b538
kedsnaik <kedsnaik@gmail.com>
2019-12-02 00:50:22 +0530

    Updates

---
+++ precache-manifest.f25b369cc28aa0360e8b2aca7d0c697b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c1eaa8647023b5adf16c344ead8430",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5369032336315ee8b478",
+    "url": "/portfolio/static/css/main.3fb7c67f.chunk.css"
+  },
+  {
+    "revision": "a39411948c5ffe22a890",
+    "url": "/portfolio/static/js/2.24794874.chunk.js"
+  },
+  {
+    "revision": "5369032336315ee8b478",
+    "url": "/portfolio/static/js/main.281c82de.chunk.js"
+  },
+  {
+    "revision": "0c1e52f393a11816286b",
+    "url": "/portfolio/static/js/runtime-main.5a497455.js"
+  },
+  {
+    "revision": "5b1da3e6cc9383bbdaca4b7d677fb89d",
+    "url": "/portfolio/static/media/AumPic.5b1da3e6.png"
+  },
+  {
+    "revision": "edd859005fb375a8bfcf5b079948100e",
+    "url": "/portfolio/static/media/KedLogo.edd85900.png"
+  }
+]);
